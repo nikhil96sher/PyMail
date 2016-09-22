22751427c6c6994a20a023e94cc247429765650a
Anshul Shah <anshulshah96@gmail.com>
2016-09-22 11:49:23 +0530

    added list and fetching list

--- guitest.py
+++ guitest.py
@@ -1,6 +1,9 @@
 from Tkinter import *
+from ReceiveModule import *
 import tkMessageBox
 from sender import Mail
+import logging
+from conf import *
 class Compose:
 
 	def __init__(self, master, smtp_h,smtp_p,user_id,password):
@@ -35,14 +38,14 @@ class App:
 
 	def __init__(self, master):
 		self.master = master
-		master.minsize(width=700, height=200)
-		master.maxsize(width=700, height=200)
+		master.minsize(width=800, height=500)
+		master.maxsize(width=800, height=500)
 		self.smtp_h = StringVar(value="192.168.180.11")
 		self.smtp_p = IntVar(value=587)
 		self.pop3_h = StringVar(value="192.168.180.11")
 		self.pop3_p = IntVar(value=110)
 		self.user_id = StringVar(value="an9sh.ucs2014@iitr.ac.in")
-		self.password = StringVar()
+		self.password = StringVar(value=passw)
 		Label(master, text="SMTP Host").grid(row=0)
 		Label(master, text="SMTP Port").grid(row=1)
 		Label(master, text="POP3 Host").grid(row=2)
@@ -55,28 +58,49 @@ class App:
 		e4 = Entry(master, textvariable=self.pop3_p ,width=50).grid(row=3, column=1)
 		e5 = Entry(master, textvariable=self.user_id ,width=50).grid(row=4, column=1)
 		e6 = Entry(master, textvariable=self.password , show="*",width=50).grid(row=5, column=1)
-		self.retrieve_button = Button(master, text="Retrive Mails", command=self.retrive)
-		self.retrieve_button.grid(row=6,column=0)
-		self.compose_button = Button(master, text="Compose", command=self.send)
-		self.compose_button.grid(row=6,column=1)
-		self.quit_button = Button(master, text="QUIT", fg="red", command=master.quit)
-		self.quit_button.grid(row=6,column=2)
 
-	def send(self):
+		self.lb = Listbox(master, name='lb', height = 20, width = 70)
+		self.lb.bind('<<ListboxSelect>>', self.retrieve)
+		self.lb.grid(row = 6, column = 0, rowspan = 10, columnspan = 2)
+		self.text = Text(master, height = 30, width = 40, padx = 5, pady=5)
+		self.text.grid(row=0, column=2, columnspan=3, rowspan=16)
+
+		self.retrieve_button = Button(master, text="Retrieve Mails", command=self.retrieve_list)
+		self.retrieve_button.grid(row=16,column=0)
+		self.compose_button = Button(master, text="Compose", command=self.compose)
+		self.compose_button.grid(row=16,column=1)
+		self.quit_button = Button(master, text="QUIT", fg="red", command=master.quit)
+		self.quit_button.grid(row=16,column=2)
+
+	def compose(self):
 		send_app = Compose(self.master, self.smtp_h.get(), self.smtp_p.get(), self.user_id.get(), self.password.get())
 		# print "Logging to " + self.username.get() 
 		# mail = Mail("192.168.180.11", port=587, username=self.user_id.get(), password=self.password.get(),
 		# 	use_tls=False, use_ssl=False, debug_level=1)
 		# mail.send_message("TEST Message", fromaddr=self.sender_id.get(), to=self.receiver_id.get(), body=self.message.get())
 
-	def retrive(self):
-		print "Logging to " + self.username.get() 
-		mail = Mail("192.168.180.11", port=587, username=self.username.get(), password=self.password.get(),
-			use_tls=False, use_ssl=False, debug_level=1)
-		mail.send_message("TEST Message", fromaddr=self.sender_id.get(), to=self.receiver_id.get(), body=self.message.get())
+	def retrieve_list(self, lower_limit = 0, upper_limit = 10):
+		addr_list, subj_list = RetrieveList(self.pop3_h.get(), self.pop3_p.get(), self.user_id.get(), self.password.get(), logging.DEBUG)
+		self.lb.delete(0,self.lb.size()-1)
+		for  i,a in enumerate(addr_list):
+			addri =  addr_list[i].split(' ')[-1][:-1]
+			subji = " ".join( subj_list[i].split(' ')[1:] )
+			self.lb.insert(i+1,addri+" : "+subji)
 
+		self.lb.activate(1)
+
+	def retrieve(self, event):
+		self.text.delete("1.0",END)
+		w = event.widget
+		index = int(w.curselection()[0])
+		value = w.get(index)
+		body = RetrieveEmail(self.pop3_h.get(), self.pop3_p.get(), self.user_id.get(), self.password.get(), index+1, logging.DEBUG)
+		print body
+		self.text.insert(INSERT, body)
 
 if __name__ == "__main__":
+	reload(sys)  
+	sys.setdefaultencoding('utf8')
 	root = Tk()
 	app = App(root)
 	root.mainloop()
