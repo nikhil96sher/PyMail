c87ff31982f30e9b31907ed7e9dea6a6004c1178
Anshul Shah <anshulshah96@gmail.com>
2016-10-24 03:24:26 +0530

    WORKING

--- guitest.py
+++ guitest.py
@@ -1,11 +1,12 @@
 from Tkinter import *
-# from ReceiveModule import *
 from receive_module import *
 import tkMessageBox
 from sender import Mail
 import logging
 from conf import *
 import email
+import os
+
 class Compose:
 
 	def __init__(self, master, smtp_h,smtp_p,user_id,password):
@@ -42,18 +43,21 @@ class App:
 		self.master = master
 		master.minsize(width=800, height=500)
 		master.maxsize(width=800, height=500)
-		self.smtp_h = StringVar(value="192.168.180.11")
-		self.smtp_p = IntVar(value=587)
-		self.pop3_h = StringVar(value="192.168.180.11")
-		self.pop3_p = IntVar(value=110)
-		self.user_id = StringVar(value="an9sh.ucs2014@iitr.ac.in")
-		self.password = StringVar(value=passw)
+		self.smtp_h 	= StringVar(value="192.168.180.11")
+		self.smtp_p 	= IntVar(value=587)
+		self.pop3_h 	= StringVar(value="192.168.180.11")
+		self.pop3_p 	= IntVar(value=110)
+		self.user_id 	= StringVar(value="an9sh.ucs2014@iitr.ac.in")
+		self.password 	= StringVar(value=passw)
+		self.info 		= StringVar()
 		Label(master, text="SMTP Host").grid(row=0)
 		Label(master, text="SMTP Port").grid(row=1)
 		Label(master, text="POP3 Host").grid(row=2)
 		Label(master, text="POP3 Port").grid(row=3)
 		Label(master, text="Email Id").grid(row=4)
 		Label(master, text="Password").grid(row=5)
+		# Label(master, text="TXR").grid(row=18)
+		Label(master, textvariable = self.info).grid(row=18)
 		e1 = Entry(master, textvariable=self.smtp_h ,width=50).grid(row=0, column=1)
 		e2 = Entry(master, textvariable=self.smtp_p ,width=50).grid(row=1, column=1)
 		e3 = Entry(master, textvariable=self.pop3_h ,width=50).grid(row=2, column=1)
@@ -65,14 +69,18 @@ class App:
 		self.lb.bind('<<ListboxSelect>>', self.retrieve)
 		self.lb.grid(row = 6, column = 0, rowspan = 10, columnspan = 2)
 		self.text = Text(master, height = 30, width = 40, padx = 5, pady=5)
-		self.text.grid(row=0, column=2, columnspan=3, rowspan=16)
+		self.text.grid(row=0, column=2, columnspan=3, rowspan=15)
+		self.retrieve_button = Button(master, text="Open in Browser", command=self.open_in_browser)
+		self.retrieve_button.grid(row=15,column=2,columnspan=3)
 
-		self.retrieve_button = Button(master, text="Retrieve Mails", command=self.retrieve_list)
+		self.retrieve_button = Button(master, text="LOGIN", command=self.login)
 		self.retrieve_button.grid(row=16,column=0)
+		self.retrieve_button = Button(master, text="Retrieve Mails", command=self.retrieve_list)
+		self.retrieve_button.grid(row=16,column=1)
 		self.compose_button = Button(master, text="Compose", command=self.compose)
-		self.compose_button.grid(row=16,column=1)
+		self.compose_button.grid(row=16,column=2)
 		self.quit_button = Button(master, text="QUIT", fg="red", command=master.quit)
-		self.quit_button.grid(row=16,column=2)
+		self.quit_button.grid(row=16,column=3)
 
 	def compose(self):
 		send_app = Compose(self.master, self.smtp_h.get(), self.smtp_p.get(), self.user_id.get(), self.password.get())
@@ -81,36 +89,40 @@ class App:
 		# 	use_tls=False, use_ssl=False, debug_level=1)
 		# mail.send_message("TEST Message", fromaddr=self.sender_id.get(), to=self.receiver_id.get(), body=self.message.get())
 
+	def login(self):
+		pop_obj = pop3lib(self.pop3_h.get(), self.pop3_p.get(), self.user_id.get(), self.password.get())
+		self.pop_obj = pop_obj
+		if not pop_obj.connection:
+			self.info.set("Connection Not Possible")
+		elif not pop_obj.username_valid:
+			self.info.set("Incorrect Username")
+		elif not pop_obj.password_valid:
+			self.info.set("Incorrect Password")
+		else:
+			self.info.set("Login Successful")
+			
+	def open_in_browser(self):
+		os.system("google-chrome message_retrieved.html")
+
 	def retrieve_list(self, lower_limit = 0, upper_limit = 10):
-		pop3_obj = pop3lib()
-		addr_list, subj_list = RetrieveList(self.pop3_h.get(), self.pop3_p.get(), self.user_id.get(), self.password.get(), logging.DEBUG)
+		if not hasattr(self, 'pop_obj'):
+			self.info.set("Please Login First")
+			return 
+		addr_list, subj_list = self.pop_obj.get_message_list()
 		self.lb.delete(0,self.lb.size()-1)
 		for i,a in enumerate(addr_list):
 			addri =  addr_list[i].split(' ')[-1][:-1]
 			subji = " ".join( subj_list[i].split(' ')[1:] )
 			self.lb.insert(i+1,addri+" : "+subji)
 		self.lb.activate(1)
+		self.info.set(str(self.pop_obj.message_count) + " Messages Retrieved")
 
 	def retrieve(self, event):
 		self.text.delete("1.0",END)
 		w = event.widget
 		index = int(w.curselection()[0])
 		value = w.get(index)
-		body = RetrieveEmail(self.pop3_h.get(), self.pop3_p.get(), self.user_id.get(), self.password.get(), index+1, logging.DEBUG)
-		body = '\n'.join(body.split('\n')[1:])
-		# print body
-		msg = body
-		b = email.message_from_string(msg)
-		body = ""
-		if b.is_multipart():
-		    for payload in b.get_payload():
-		        body = body + payload.get_payload()
-		else:
-		    body = b.get_payload()
-		body = body.replace("\r","")
-		# body = '\n'.join(body.split('\n')[:-1])
-		body = body[:-2]
-		body = body[:body.rfind("\r\n.")]
+		body = self.pop_obj.get_email_body(index)
 		self.text.insert(INSERT, body)
 
 if __name__ == "__main__":
--- receive_module.py
+++ receive_module.py
@@ -18,54 +18,153 @@ login = username
 password = passw
 host = host_addr
 CRLF = '\r\n'
+TERMINATOR = CRLF + '.' + CRLF
+
+def decode(input_str):
+	result = ''
+	search_result = re.search('=\?([^\?]*)\?([^\?]*)\?([^\?]*)\?=', input_str)
+	while search_result is not None:
+		charset, tp, text = search_result.groups()
+		s = search_result.start(0)
+		e = search_result.end(0)
+		text = text.encode('cp866', 'ignore').decode('cp866', 'ignore')
+		result += input_str[:s]
+		input_str = input_str[e:].lstrip()
+		if tp.lower() != 'q':
+			result += base64.b64decode(text.encode('cp866')).decode(charset, 'ignore')
+		else:
+			result += quopri.decodestring(text).decode(charset, 'ignore')
+		search_result = re.search('=\?([^\?]*)\?([^\?]*)\?([^\?]*)\?=', input_str)
+	else:
+		result += input_str
+	return result
 
 class pop3lib:
 
 	message_list = []
 
-	def send_message(self,m, s):
+	def send_message(self,m):
 		logging.debug("\nC: "+m)
-		s.send((m + '\r\n').encode('utf-8'))
+		self.sock.send((m + '\r\n').encode('utf-8'))
 
-	def send_password(self,m,s):
+	def send_password(self,m):
 		logging.debug("\nC: *********")
-		s.send((m + '\r\n').encode('utf-8'))
+		self.sock.send((m + '\r\n').encode('utf-8'))
 
-	def send_and_receiveline(self,mes,s):
+	def send_and_receiveline(self,mes):
 		logging.debug("\nC: "+mes)
-		s.send((mes + '\r\n').encode('utf-8'))
-		line = s.recv(2048)
+		self.sock.send((mes + '\r\n').encode('utf-8'))
+		line = self.sock.recv(2048)
 		logging.debug("\nS: " + line)
 		return line
 	
-	def receiveline(self,s):
-		line = s.recv(2048)
+	def receiveline(self):
+		line = self.sock.recv(2048)
 		logging.debug("\nS: " + line)
 		return line
+
+	def receive_till_term(self, terminator):
+		response = self.sock.recv(2048)
+		while not response.endswith(TERMINATOR) and not response.endswith('.\r\n') and not response.endswith('.\r\n\r\n'):
+			new_response = self.sock.recv(2048)
+			response += new_response
+			print new_response
+		return response
+
+	def get_message_count(self):
+		self.send_message("list")
+		mes = self.receive_till_term(TERMINATOR)
+		cnt = len(mes.split('\r\n')) - 3
+		return cnt
+
+	def get_result(self,data):
+		addr = 'No \'From: ...\''
+		subj = 'No \'Subject: ...\''
+		data = data.decode('cp866', 'ignore')
+		from_data = re.search('^From:.*(.*\r?\n\s.*)*$', data, re.M | re.I)
+		if from_data is not None:
+			addr = decode(from_data.group(0))
+		subj_data = re.search('^Subject:.*(.*\r?\n\s.*)*', data, re.M | re.I)
+		if subj_data is not None:
+			subj = decode(subj_data.group(0))
+		return [subj, addr]
 
 	def __init__(self, host_name, host_port, user_id, passw, log_level=logging.DEBUG):
 		logging.basicConfig(format='%(levelname)s:%(message)s', level=log_level)
 		try:
 			sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			self.sock = sock
 			sock.connect((host_name, host_port))
+			self.connection = True
 		except Exception as e:
 			print('can\'t connect to {0} on {1} port \r\n{3}'.format(host, port, e.__repr__()))
-			sys.exit(0)
+			self.connection = False
+			return
 
-		# print sock.recv(2048)		
-
-		self.server_info = self.receiveline(sock)
+		self.server_info = self.receiveline()
 		auto = [
 			'user {0}'.format(user_id),
 			'pass {0}'.format(passw),
 		]
 
+		response = []
 		for m in auto:
-			self.send_password(m, sock)
+			self.send_password(m)
+			response.append(self.receiveline())
 
-		print "DONE"
-						
+		print response
+
+		if response[0].find("+OK Name is a valid mailbox") != -1:
+			self.username_valid = True
+		else:
+			self.username_valid = False
+
+		if response[1].find("+OK Maildrop ready") != -1:
+			self.password_valid = True
+		else:
+			self.password_valid = False
+
+	def get_message_list(self):
+		self.message_count = self.get_message_count()
+		addr_list = []
+		subj_list = []
+
+		for message_number in range(1,self.message_count+1):
+			self.send_message('top {0} 0'.format(message_number))
+			# response = self.receive_till_term(TERMINATOR)
+
+			response = self.sock.recv(2048)
+			while not response.endswith(b'\r\n.\r\n'):
+				response += self.sock.recv(2048)
+
+			subj, addr = self.get_result(response)
+			subj_list.append(subj)
+			addr_list.append(addr)
+			logging.info(('\nS: {0}\n{1}\n'.format(addr, subj)))
+		return addr_list, subj_list
+
+
+	def get_email_body(self, index):
+		self.send_message("RETR "+str(index))
+		response = self.receive_till_term(TERMINATOR)
+		response = '\n'.join(response.split('\n')[1:])
+		b = email.message_from_string(response)
+		body = ""
+		if b.is_multipart():
+			for payload in b.get_payload():
+				body = body + payload.get_payload()
+		else:
+			body = b.get_payload()
+		body = body.replace("\r","")
+		body = body[:-2]
+		f =  open("message_retrieved.html","w")
+		f.write(body)
+		return body
+
 if __name__ == "__main__":
 	reload(sys)  
 	sys.setdefaultencoding('utf8')
 	pop_obj = pop3lib(host,port,username,password)
+	# print pop_obj.get_message_list()
+	# print pop_obj.get_email_body(14)
+	print "DONE"
